7adcfb3c9731629ce05b53abed525933db685b5b
Denis Washington <denisw@online.de>
2012-05-26 19:09:15 +0200

    Add 'max' and 'after' query parameters to node feed retrieval requests
    'after' is not supported for non-buddycloud nodes because XEP-0060
    doesn't support this feature (it just has 'max_items'). The buddycloud
    server has support for Result Set Management (XEP-0059) for node
    item requests, though, so everything works there.

--- test/node_feed.js
+++ test/node_feed.js
@@ -49,7 +49,77 @@ var mockConfig = {
                    <content>two</content>\
                  </entry>\
                </item>\
+               <item id="3">\
+                 <entry xmlns="http://www.w3.org/2005/Atom">\
+                   <id>3</id>\
+                   <content>three</content>\
+                 </entry>\
+               </item>\
              </items>\
+           </pubsub>\
+         </iq>',
+
+        '<iq from="alice@localhost/http" type="get">\
+           <pubsub xmlns="http://jabber.org/protocol/pubsub">\
+             <items node="/user/alice@localhost/posts"/>\
+             <set xmlns="http://jabber.org/protocol/rsm">\
+               <max>2</max>\
+             </set>\
+           </pubsub>\
+         </iq>':
+        '<iq type="result">\
+           <pubsub xmlns="http://jabber.org/protocol/pubsub">\
+             <items node="/user/alice@localhost/posts">\
+               <item id="1">\
+                 <entry xmlns="http://www.w3.org/2005/Atom">\
+                   <id>1</id>\
+                   <content>one</content>\
+                 </entry>\
+               </item>\
+               <item id="2">\
+                 <entry xmlns="http://www.w3.org/2005/Atom">\
+                   <id>2</id>\
+                   <content>two</content>\
+                 </entry>\
+               </item>\
+             </items>\
+             <set xmlns="http://jabber.org/protocol/rsm">\
+               <first>1</first>\
+               <last>2</last>\
+               <count>3</count>\
+             </set>\
+           </pubsub>\
+         </iq>',
+
+        '<iq from="alice@localhost/http" type="get">\
+           <pubsub xmlns="http://jabber.org/protocol/pubsub">\
+             <items node="/user/alice@localhost/posts"/>\
+             <set xmlns="http://jabber.org/protocol/rsm">\
+               <after>1</after>\
+             </set>\
+           </pubsub>\
+         </iq>':
+        '<iq type="result">\
+           <pubsub xmlns="http://jabber.org/protocol/pubsub">\
+             <items node="/user/alice@localhost/posts">\
+               <item id="2">\
+                 <entry xmlns="http://www.w3.org/2005/Atom">\
+                   <id>2</id>\
+                   <content>two</content>\
+                 </entry>\
+               </item>\
+               <item id="3">\
+                 <entry xmlns="http://www.w3.org/2005/Atom">\
+                   <id>3</id>\
+                   <content>three</content>\
+                 </entry>\
+               </item>\
+             </items>\
+             <set xmlns="http://jabber.org/protocol/rsm">\
+               <first>2</first>\
+               <last>3</last>\
+               <count>3</count>\
+             </set>\
            </pubsub>\
          </iq>',
 
@@ -116,7 +186,43 @@ describe('Node Feed', function() {
                 atom.get(e2, 'atom:id').text().should.equal('2');
                 atom.get(e2, 'atom:content').text().should.equal('two');
                 should.exist(atom.get(e2, 'atom:title'));
+                var e3 = entries[2];
+                atom.get(e3, 'atom:id').text().should.equal('3');
+                atom.get(e3, 'atom:content').text().should.equal('three');
+                should.exist(atom.get(e3, 'atom:title'));
                 
+                done();
+            }).on('error', done);
+        });
+
+        it('should allow limiting the number of returned items', function(done) {
+            var options = {
+                path: '/channels/alice@localhost/posts?max=2',
+                auth: 'alice@localhost/http:alice'
+            };
+            tutil.get(options, function(res, body) {
+                res.statusCode.should.equal(200);
+                var feed = xml.parseXmlString(body);
+                var entries = feed.find('/a:feed/a:entry', {a: atom.ns});
+                entries.length.should.equal(2);
+                atom.get(entries[0], 'atom:id').text().should.equal('1');
+                atom.get(entries[1], 'atom:id').text().should.equal('2');
+                done();
+            }).on('error', done);
+        });
+
+        it('should allow specifying the first returned item', function(done) {
+            var options = {
+                path: '/channels/alice@localhost/posts?after=1',
+                auth: 'alice@localhost/http:alice'
+            };
+            tutil.get(options, function(res, body) {
+                res.statusCode.should.equal(200);
+                var feed = xml.parseXmlString(body);
+                var entries = feed.find('/a:feed/a:entry', {a: atom.ns});
+                entries.length.should.equal(2);
+                atom.get(entries[0], 'atom:id').text().should.equal('2');
+                atom.get(entries[1], 'atom:id').text().should.equal('3');
                 done();
             }).on('error', done);
         });
