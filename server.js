7003d98ec2563192c88e9090e63a4d6080ef5ffd
Denis Washington <denisw@online.de>
2012-08-18 10:29:51 +0200

    Add Access-Control-Allow-Methods header to responses
    This allows cross-origin POST, PUT and DELETE requests.

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ function crossOriginAllower(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
     if (req.method == 'OPTIONS') {
         res.send(200);
