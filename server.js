b46ecfa503adf839e77ba1bba2896e6afc12313a
Denis Washington <denisw@online.de>
2012-08-23 07:56:33 +0200

    Allow X-Requested-With header on cross-origin requests
    This is required to make cross-origin requests work in Chrome.

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function crossOriginAllower(req, res, next) {
 
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
     if (req.method == 'OPTIONS') {
