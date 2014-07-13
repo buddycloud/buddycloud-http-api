9d1ca586df1c65777e6b59e995f9a1be17aed53e
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-07-13 17:08:38 +0100

    Wrap xmpp-ftw up into the API server

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ var session = require('./src/util/session');
 var grip = require('./src/util/grip');
 var SegfaultHandler = require('segfault-handler');
 
+var Emitter    = require('primus-emitter')
+  , Primus     = require('primus')
+  , xmpp       = require('xmpp-ftw')
+  , Buddycloud = require('xmpp-ftw-buddycloud');
+
 // Watch for segfaults
 SegfaultHandler.registerHandler();
 
@@ -35,6 +40,7 @@ function setupConfig(app) {
           console.log("Incoming request: " + req.method + " " + req.url)
         next()
     })
+    app.use(express.static(__dirname + '/public'))
     app.use(auth.parser);
     app.use(grip.parser);
     app.use(crossOriginAllower);
@@ -120,7 +126,34 @@ var app = createServer();
 setupConfig(app);
 setupResourceHandlers(app);
 printInitialMessage();
-app.listen(config.port);
+var server = app.listen(config.port);
+
+var options = {
+    transformer: 'socket.io',
+    parser: 'JSON',
+    transports: [
+        'websocket',
+        'htmlfile',
+        'xhr-polling',
+        'jsonp-polling'
+    ]
+}
+
+var primus = new Primus(server, options)
+primus.use('emitter', Emitter)
+primus.save(__dirname + '/public/scripts/primus.js')
+
+primus.on('connection', function(socket) {
+    console.log('Websocket connection made')
+    var xmppFtw = new xmpp.Xmpp(socket)
+    xmppFtw.addListener(new Buddycloud())
+    socket.xmppFtw = xmppFtw
+})
+
+primus.on('disconnection', function(socket) {
+    console.log('Client disconnected, logging them out')
+    socket.xmppFtw.logout()
+})
 
 process.on('uncaughtException', function(error) {
     // Try and prevent issues crashing the whole system 
