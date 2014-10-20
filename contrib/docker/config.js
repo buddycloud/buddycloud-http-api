78c571f9200ec6717ec1895dbde29cc6e9bb7ea5
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-20 10:11:31 +0100

    Rework config file

--- contrib/docker/config.js
+++ contrib/docker/config.js
@@ -3,36 +3,27 @@ exports._ = {
   port: 9123,
 };
 
+if (!process.env.XMPP_DOMAIN) {
+    throw new Error('Missing XMPP domain')
+}
+if (!process.env.XMPP_HOST) {
+    throw new Error('Missing XMPP host')
+}
+if (!process.env.CHANNEL_COMPONENT) {
+    throw new Error('Missing channel server component')
+}
+
 // Production settings
 exports.production = {
-  // More logs?
-  debug: process.env('DEBUG') || false,
-
-  // the domain your run buddycloud for (if your username is name@EXAMPLE.COM then this would be EXAMPLE.COM)
-  xmppDomain: process.env('XMPP_DOMAIN') || throw new Error('Missing XMPP domain'),
-
-  // the host where your XMPP server is running
-  xmppHost: process.env('XMPP_HOST') || throw new Error('Missing XMPP host'),
- 
-  // the anonymous domain that unauthenticated users are dropped into (should match your XMPP server)
-  xmppAnonymousDomain: process.env('ANONYMOUS_COMPONENT'),
-
-  // your buddycloud channel server
-  channelDomain: process.env('CHANNEL_COMPONENT') || throw new Error('Missing channel server component'),
-
-  // the pusher component sends emails to users (https://github.com/buddycloud/buddycloud-pusher)
-  pusherComponent: process.env('PUSHER_COMPONENT'),
-
-  // the friend finder component looks for your friends from other social networks
-  friendFinderComponent: process.env('FRIENDFINDER_COMPONENT'),
-
-  // you probably want to use the buddycloud search service
-  searchComponent: process.env('SEARCH_COMPONENT'),
-
-  // where to forward media requests
-  homeMediaRoot: process.env('MEDIA_ENDPOINT'),
-
+  debug: process.env.DEBUG || false,
+  xmppDomain: process.env.XMPP_DOMAIN,
+  xmppHost: process.env.XMPP_HOST,
+  xmppAnonymousDomain: process.env.ANONYMOUS_COMPONENT,
+  channelDomain: process.env.CHANNEL_COMPONENT,
+  pusherComponent: process.env.PUSHER_COMPONENT,
+  friendFinderComponent: process.env.FRIENDFINDER_COMPONENT,
+  searchComponent: process.env.SEARCH_COMPONENT,
+  homeMediaRoot: process.env.MEDIA_ENDPOINT,
   createUserOnSessionCreation: true,
-    
-  disableWebsocket: (typeof process.env('DISABLE_WEBSOCKET') !== 'undefined') ? process.env('DISABLE_WEBSOCKET') || false
+  disableWebsocket: (typeof process.env.DISABLE_WEBSOCKET !== 'undefined') ? process.env.DISABLE_WEBSOCKET : false
 }
